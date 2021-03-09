4082b8b6b1ee8698057064344f76c88d3e4b1143
Naldo Madeira <naldomadeira@gmail.com>
2021-03-09 13:37:11 -0300

    load the models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+// temp
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Teste da aplicação' }));
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Naldo Madeira',
+        email: 'naldomadeira@gmail.com',
+        password_hash: '1234567',
+    });
+    res.json(user);
+});
 
 export default routes;
