75d4a210774c36ce940859c517af769cb79f298c
Naldo Madeira <naldomadeira@gmail.com>
2021-03-09 19:51:15 -0300

    creation UserController and modification user model

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express';
-
-// temp
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Naldo Madeira',
-        email: 'naldomadeira@gmail.com',
-        password_hash: '1234567',
-    });
-    res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
